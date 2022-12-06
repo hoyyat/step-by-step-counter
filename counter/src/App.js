93b6c8f025d7607594f1aaaab19a3aff9a05cfac
hoya <wlsgh2018@naver.com>
2022-12-07 03:58:50 +0900

    install css library

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import styled from "styled-components"
+const AppDiv = styled.div`
+  font-size: 1.5em;
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <AppDiv>
       <button>버튼</button>
-    </div>
+    </AppDiv>
   );
 }
 
